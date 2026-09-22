372536051926cdb1c707a2c933d9768f4e0af77b
Oliv <82574961+OlivierKran@users.noreply.github.com>
2026-09-22 10:07:55 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+gsap.to(".box", {
+    x: 500,
+    y: -200,
+    duration: 5,
+    rotate: 360,
+    backgroundColor: "#004b73",
+    repeat: 1,
+    yoyo: true,
+});
+
+gsap.to(".circle", { 
+    x: 540, 
+    backgroundColor: 'blue',
+    duration: 20, 
+});
+
+gsap.from(".circle2", { 
+    x: 540, 
+    backgroundColor: 'purple',
+    duration: 20,
+});
+
+gsap.fromTo(".circle3", { 
+    y: 100,
+    x: 800,
+    backgroundColor: 'orange',
+    duration: 20,
+}, { 
+    y: -540, 
+    backgroundColor: 'red',
+    duration: 5,
+    repeat: 2,
+    yoyo: true,
+});
+
+gsap.set(".circle4", {
+    x: 1240, 
+    backgroundColor: 'pink',
+    duration: 50,
+});
